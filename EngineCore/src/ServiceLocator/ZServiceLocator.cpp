f1fa17349eba7038c38c74dceda8e4814a3fd33e
IvanHunko06 <ivan.hunko.dan@gmail.com>
2025-12-01 22:59:01 +0200

    bugfix: assert and null verifications

--- EngineCore/src/ServiceLocator/ZServiceLocator.cpp
+++ EngineCore/src/ServiceLocator/ZServiceLocator.cpp
@@ -6,8 +6,11 @@ using namespace StateEngine::EngineCore::ServiceLocator;
 ZHashMap<const TypeInfo*, void*> ZServiceLocator::registeredServices_;
 std::shared_mutex ZServiceLocator::servicesMutex_;
 
-void  ZServiceLocator::registerService(const TypeInfo* type, void* instance) {
+void ZServiceLocator::registerService(const TypeInfo* type, void* instance) {
 	std::unique_lock<std::shared_mutex> lock(servicesMutex_);
+	assert(type && "type is null");
+	assert(instance && "instance is null");
+	if (!type || !instance) return;
 
 	if (registeredServices_.contains(type)) {
 		ZLOG_ERROR("EngineCore") << "Service '" << type->name.c_str() << "' is being registered twice!";
@@ -17,11 +20,15 @@ void  ZServiceLocator::registerService(const TypeInfo* type, void* instance) {
 	registeredServices_[type] = instance;
 }
 void* ZServiceLocator::getService(const TypeInfo* type) {
+	assert(type && "type is null");
+	if (!type) return nullptr;
 	std::unique_lock<std::shared_mutex> lock(servicesMutex_);
 
 	return registeredServices_.contains(type) ? registeredServices_[type] : nullptr;
 }
 void  ZServiceLocator::removeService(const TypeInfo* type) {
+	assert(type && "type is null");
+	if (!type) return;
 	std::unique_lock<std::shared_mutex> lock(servicesMutex_);
 
 	registeredServices_.erase(type);
