4ccdb31055b5657cd4391598b61e3d08d9582685
IvanHunko06 <ivan.hunko.dan@gmail.com>
2025-12-03 22:52:35 +0200

    feature: mimalloc debug output print

--- EngineCore/src/EngineCore.cpp
+++ EngineCore/src/EngineCore.cpp
@@ -1,23 +1,31 @@
-#ifdef _WIN32
-#include <windows.h>
 #include "MemoryManagment/ZAllocator_SEMalloc_Virtual.hpp"
 #include "StringCollection/ZStringCollection.hpp"
 #include "EngineTypeSystem/ZTypeRegistry.hpp"
 #include "EventBus/ZEventBus.hpp"
-//using StateEngine::EngineCore::MemoryManagment::ZAllocator_SEMalloc_Virtual;
+#ifdef USE_MIMALLOC_ALLOCATOR
+#include "mimalloc.h"
+#endif
 using StateEngine::EngineCore::StringCollection::ZStringCollection;
 using StateEngine::EngineCore::EngineTypeSystem::ZTypeRegistry;
 using StateEngine::EngineCore::EventBus::ZEventBus;
+
+#ifdef _WIN32
+#include <windows.h>
+#ifdef USE_MIMALLOC_ALLOCATOR
+void MimallocOutput(const char* msg, void* arg) {
+	OutputDebugStringA(msg);
+}
+#endif
 BOOL APIENTRY DllMain(HMODULE hModule,
 					  DWORD  ul_reason_for_call,
 					  LPVOID lpReserved
 					 )
 {
-	if (ul_reason_for_call == DLL_PROCESS_DETACH) {
-		//ZStringCollection::release();
-		//ZAllocator_SEMalloc_Virtual::releaseAllArenas();
-	}
-	else if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+#ifdef USE_MIMALLOC_ALLOCATOR
+		mi_register_output(MimallocOutput, nullptr);
+		mi_option_enable(mi_option_show_stats);
+#endif
 		ZStringCollection::init();
 		ZTypeRegistry::RegisterBaseTypes();
 		ZEventBus::RegisterBaseEvents();
