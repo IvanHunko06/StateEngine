9c4c33763f631583900d545ac5753026d579d27a
IvanHunko06 <ivan.hunko.dan@gmail.com>
2025-12-14 17:33:16 +0200

    feature: Improved allocator compatibility with structures

--- EngineCore/include/EngineCore/DataStructures/EastlAllocator.hpp
+++ EngineCore/include/EngineCore/DataStructures/EastlAllocator.hpp
@@ -4,10 +4,12 @@
 namespace StateEngine::EngineCore::DataStructures {
 
 	struct EastlAllocator {
+		const char* name{ nullptr };
 		using size_type = size_t;
 		using difference_type = ptrdiff_t;
 		EastlAllocator() noexcept = default;
-		EastlAllocator(const char* = "Global Allocator") noexcept {}
+		EastlAllocator(const char*) noexcept {}
+		EastlAllocator(void*){}
 
 		void* allocate(size_t n, int flags = 0) {
 			return MemoryAllocator_AlignedAllocate(n, alignof(std::max_align_t));
@@ -20,6 +22,8 @@ namespace StateEngine::EngineCore::DataStructures {
 			MemoryAllocator_Deallocate(p);
 		}
 
+		void set_name(const char* pName) { name = pName; }
+		const char* get_name() const noexcept { return name; }
 		bool operator==(const EastlAllocator&) const noexcept { return true; }
 		bool operator!=(const EastlAllocator&) const noexcept { return false; }
 	};
